1d7654c6687232e839e2a24a232760a101a1067b
CG.Lee <contact@chulgil.me>
2019-06-01 01:30:22 +0900

    add NodeView class

---
+++ Assets/Scripts/NodeView.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeView : MonoBehaviour
+{
+    public GameObject tile;
+
+    [Range(0, 0.5f)]
+    public float boaderSize = 0.15f;
+
+    public void Init(Node node)
+    {
+        if(tile != null)
+        {
+            gameObject.name = "Node (" + node.xIndex + "," + node.yIndex + ")";
+            gameObject.transform.position = node.position;
+            tile.transform.localScale = new Vector3(1f - boaderSize, 1f, 1f - boaderSize);
+        }
+    }
+
+    void ColorNode(Color color, GameObject go)
+    {
+        if (go != null)
+        {
+            Renderer goRenderer = go.GetComponent<Renderer>();
+
+            if (goRenderer != null)
+            {
+                goRenderer.material.color = color;
+            }
+        }
+    }
+
+    public void ColorNode(Color color)
+    {
+        ColorNode(color, tile);
+    }
+}
